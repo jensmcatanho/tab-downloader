8a5efca3a6417fa97402ec98fd3a836a678c694d
Jean Catanho <jensmcatanho@gmail.com>
2017-12-10 00:59:41 -0200

    main: add a function to download files if they don't exist

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -17,9 +20,55 @@ func main() {
 
 	doc.Find("tr .tr__lg").Not(".tr__active").Each(func(i int, s *goquery.Selection) {
 		if !s.Find("a").HasClass("song js-tp_link") {
-			name := s.Find("a").Text()
-			link, _ := s.Find("a").Attr("href")
-			fmt.Println(name, link)
+			tab := s.Find("a")
+			name := tab.Text()
+			log.Printf("Processing %v tab...", name)
+			tabURL, _ := tab.Attr("href")
+
+			doc, err := goquery.NewDocument(tabURL)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			doc.Find("div").Each(func(i int, s *goquery.Selection) {
+				if s.HasClass("textversbox") {
+					tabID, _ := s.Find("input").Attr("value")
+					downloadFile(name, tabID, tabURL)
+				}
+			})
 		}
 	})
+}
+
+func downloadFile(filename, id, referer string) {
+	if _, err := os.Stat(fmt.Sprint("avantasia/", filename)); err == nil {
+		log.Println("File already exists. Fast-forwarding...")
+		return
+	}
+
+	out, err := os.Create(fmt.Sprint("avantasia/", filename))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://tabs.ultimate-guitar.com/tab/download?id=%v", id), nil)
+	req.Header.Add("Host", "tabs.ultimate-guitar.com")
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0")
+	req.Header.Add("Accept-Language", "pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3")
+	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Add("Referer", referer)
+	req.Header.Add("Connection", "keep-alive")
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
